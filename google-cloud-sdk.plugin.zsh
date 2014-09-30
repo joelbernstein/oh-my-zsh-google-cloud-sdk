14fdeae387cee84d82b2a6c3b67490193f108adb
Joel Bernstein <joel@fysh.org>
2014-09-30 02:28:02 +0200

    correct oh-my-zsh plugin name

--- google-cloud-sdk.plugin.zsh
+++ google-cloud-sdk.plugin.zsh
@@ -3,7 +3,6 @@ if [ -d "$HOME/bin" -a -d "$HOME/bin/google-cloud-sdk" ]; then
   source "$HOME/bin/google-cloud-sdk/path.zsh.inc"
   # enable zsh completion for gcloud.
   source "$HOME/bin/google-cloud-sdk/completion.zsh.inc"
-  echo "gcloud"
 else
   echo "error: expected to find Google Cloud SDK in ~/bin/google-cloud-sdk"
 fi
