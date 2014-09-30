4ff1e22d039b508d3ca4ff1f5f5be83e84cf1027
Joel Bernstein <joel@fysh.org>
2014-09-30 02:18:42 +0200

    initial import

---
+++ google-cloud-sdk.zsh
@@ -0,0 +1,9 @@
+if [ -d "$HOME/bin" -a -d "$HOME/bin/google-cloud-sdk" ]; then
+  # update PATH for the Google Cloud SDK.
+  source "$HOME/bin/google-cloud-sdk/path.zsh.inc"
+  # enable zsh completion for gcloud.
+  source "$HOME/bin/google-cloud-sdk/completion.zsh.inc"
+  echo "gcloud"
+else
+  echo "error: expected to find Google Cloud SDK in ~/bin/google-cloud-sdk"
+fi
