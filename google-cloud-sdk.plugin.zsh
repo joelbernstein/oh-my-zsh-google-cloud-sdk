f4136a3b0d13bab6f787de7eb4764f198c6f3f97
Joel Bernstein <joel@fysh.org>
2019-06-06 04:34:46 +0200

    initial commit

--- google-cloud-sdk.plugin.zsh
+++ google-cloud-sdk.plugin.zsh
@@ -1,8 +1,6 @@
-if [ -d "$HOME/bin" -a -d "$HOME/bin/google-cloud-sdk" ]; then
-  # update PATH for the Google Cloud SDK.
-  source "$HOME/bin/google-cloud-sdk/path.zsh.inc"
-  # enable zsh completion for gcloud.
-  source "$HOME/bin/google-cloud-sdk/completion.zsh.inc"
+if [ -d "/usr/local/Caskroom/google-cloud-sdk/latest" ]; then
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 else
-  echo "error: expected to find Google Cloud SDK in ~/bin/google-cloud-sdk"
+  echo "error: expected to find Google Cloud SDK in /usr/local/Caskroom/google-cloud-sdk/latest"
 fi
